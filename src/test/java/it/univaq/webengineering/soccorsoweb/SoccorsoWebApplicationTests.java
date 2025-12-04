1f10c25e1f1bd7b8048f52d918699848aa1d6fb2
Francesco <77879644+VampaFrancesco@users.noreply.github.com>
2025-12-04 22:15:49 +0100

    setup progetto

---
+++ src/test/java/it/univaq/webengineering/soccorsoweb/SoccorsoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.webengineering.soccorsoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccorsoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
